05e7acca5a9ea75038f8e9b9406313bd0931a854
FrancescaCapodanno <capofrancy@gmail.com>
2022-11-27 16:39:06 +0100

    created customer model

---
+++ src/main/java/com/model/Customer.java
@@ -0,0 +1,36 @@
+package com.model;
+
+public class Customer {
+
+    private final String firstName;
+    private final String lastName;
+    private final String fiscalCode;
+    private String address;
+
+    public Customer(String firstName, String lastName, String fiscalCode, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fiscalCode = fiscalCode;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
